8e2795cf95d73aeb9721ba34e4471290dcf57b17
nyounh notebook <nyounh@gmail.com>
2018-07-01 20:26:35 +0900

    change status bar

--- jungnami/app/src/main/java/sopt_jungnami_android/jungnami/MainActivity.kt
+++ jungnami/app/src/main/java/sopt_jungnami_android/jungnami/MainActivity.kt
@@ -1,13 +1,29 @@
 package sopt_jungnami_android.jungnami
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setStatusBarColor()
 
+
+    }
+
+
+    private fun setStatusBarColor(){
+        val view : View? = window.decorView
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (view != null){
+                view.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                window.statusBarColor = Color.parseColor("#FFFFFF")
+            }
+        }
     }
 }
