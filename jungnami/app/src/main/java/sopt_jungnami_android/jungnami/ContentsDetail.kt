df05f973039b03c6133786f7cd2ddb84223b8956
sooyounglim <stellar1198@gmail.com>
2018-07-02 19:00:29 +0900

    create contents_detail.xml & .kt and modify writepage.xml

---
+++ jungnami/app/src/main/java/sopt_jungnami_android/jungnami/ContentsDetail.kt
@@ -0,0 +1,12 @@
+package sopt_jungnami_android.jungnami
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ContentsDetail : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contents_detail)
+    }
+}
