1c756f0e42988a277aeaa0f8f24791133adb5de7
sooyounglim <stellar1198@gmail.com>
2018-07-02 15:09:43 +0900

    modify community_writepage.xml and .kt

--- jungnami/app/src/main/java/sopt_jungnami_android/jungnami/CommunityWritePage.kt
+++ jungnami/app/src/main/java/sopt_jungnami_android/jungnami/CommunityWritePage.kt
@@ -2,8 +2,22 @@ package sopt_jungnami_android.jungnami
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_community_write_page.*
 
-class CommunityWritePage : AppCompatActivity() {
+class CommunityWritePage : AppCompatActivity(), View.OnClickListener {
+
+    override fun onClick(v: View?) {
+        when(v) {
+            community_act_writepage_back_iv -> {
+                finish()
+            }
+            community_act_writepage_complete_iv -> {
+                community_act_writepage_complete_iv.isSelected = false
+
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
