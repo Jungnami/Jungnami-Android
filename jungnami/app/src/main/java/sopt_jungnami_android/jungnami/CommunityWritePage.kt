71a16f64d44df4a0ca51f7e86658187c85ce65eb
sooyounglim <stellar1198@gmail.com>
2018-07-02 14:25:00 +0900

    create community_writepage.xml

---
+++ jungnami/app/src/main/java/sopt_jungnami_android/jungnami/CommunityWritePage.kt
@@ -0,0 +1,12 @@
+package sopt_jungnami_android.jungnami
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CommunityWritePage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_community_write_page)
+    }
+}
