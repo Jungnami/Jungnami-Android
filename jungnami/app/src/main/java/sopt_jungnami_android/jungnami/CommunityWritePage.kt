0ba111394f9644d385ddcea5fd8c60a2de1786b0
sooyounglim <stellar1198@gmail.com>
2018-07-03 13:22:35 +0900

    write codes how to connect mobile gallary of users and create mypage_alarm // recyclerview

--- jungnami/app/src/main/java/sopt_jungnami_android/jungnami/CommunityWritePage.kt
+++ jungnami/app/src/main/java/sopt_jungnami_android/jungnami/CommunityWritePage.kt
@@ -1,12 +1,29 @@
 package sopt_jungnami_android.jungnami
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_community_write_page.*
+import okhttp3.MediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import org.jetbrains.anko.toast
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.InputStream
 
 class CommunityWritePage : AppCompatActivity(), View.OnClickListener {
+
+    private val REQ_CODE_SELECT_IMAGE = 100
+    lateinit var data : Uri
+    private var image : MultipartBody.Part? = null
 
     override fun onClick(v: View?) {
         when(v) {
@@ -17,15 +34,57 @@ class CommunityWritePage : AppCompatActivity(), View.OnClickListener {
                 community_act_writepage_complete_iv.isSelected = false // 글, 사진, gif 중 어떤 것 하나라도 올라온다면 (조건문 처리)
                 finish()
             }
+            community_act_writepage_upload_pic_iv -> {
+                changeImage()
+            }
         }
     }
-    //아래 16
+
+    fun changeImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = android.provider.MediaStore.Images.Media.CONTENT_TYPE
+        intent.data = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        startActivityForResult(intent, REQ_CODE_SELECT_IMAGE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQ_CODE_SELECT_IMAGE) {
+            if (resultCode == Activity.RESULT_OK) {
+                try {
+                    this.data = data!!.data
+                    Log.v("이미지", this.data.toString())
+
+                    val options = BitmapFactory.Options()
+
+                    var input: InputStream? = null
+                    try {
+                        input = contentResolver.openInputStream(this.data)
+                    } catch (e: FileNotFoundException) {
+                        e.printStackTrace()
+                    }
+
+                    val bitmap = BitmapFactory.decodeStream(input, null, options)
+                    val baos = ByteArrayOutputStream()
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos)
+                    val photoBody = RequestBody.create(MediaType.parse("image/jpg"), baos.toByteArray())
+                    val photo = File(this.data.toString())
+
+                    image = MultipartBody.Part.createFormData("photo", photo.name, photoBody)
+
+                    Glide.with(this).load(data.data).centerCrop().into(write_image)
+
+                    catch (e: Exception){
+                        toast("이미지 로딩에 오류가 있습니다.")
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_community_write_page)
-
-        // val handler = Handler()
-        // handler.postDelayed(community_act_writepage_gif_bubble_iv, 2000)
     }
 }
+
