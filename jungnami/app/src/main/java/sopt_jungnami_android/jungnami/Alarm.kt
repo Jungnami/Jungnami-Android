00a69ce7c61e1857917b96162a914510cec56079
sooyounglim <stellar1198@gmail.com>
2018-07-03 13:39:26 +0900

    modify Alarm.xml and .kt name

--- jungnami/app/src/main/java/sopt_jungnami_android/jungnami/Alarm.kt
+++ jungnami/app/src/main/java/sopt_jungnami_android/jungnami/Alarm.kt
@@ -3,10 +3,10 @@ package sopt_jungnami_android.jungnami
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MypageAlarmList : AppCompatActivity() {
+class Alarm : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mypage_alarm_list)
+        setContentView(R.layout.activity_alarm)
     }
 }
