fb7d855e0a92b16925e5aaafe29c6ef95d089502
nyounh notebook <nyounh@gmail.com>
2018-07-03 14:31:09 +0900

    mypage modify ver01

--- jungnami/app/src/main/java/sopt_jungnami_android/jungnami/contents/ContentsFragment.kt
+++ jungnami/app/src/main/java/sopt_jungnami_android/jungnami/contents/ContentsFragment.kt
@@ -5,7 +5,10 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_contents.*
+import org.jetbrains.anko.support.v4.startActivity
 import sopt_jungnami_android.jungnami.R
+import sopt_jungnami_android.jungnami.mypage.MyPageActivity
 
 
 class ContentsFragment : Fragment() {
@@ -16,4 +19,10 @@ class ContentsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_contents, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        contents_frag_top_bar_my_page_btn.setOnClickListener {
+            startActivity<MyPageActivity>()
+        }
+    }
 }
