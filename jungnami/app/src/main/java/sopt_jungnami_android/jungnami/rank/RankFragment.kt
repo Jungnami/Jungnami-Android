7c13e8d93fcd7499ab999df0299a9f2f9e7b582e
nyounh notebook <nyounh@gmail.com>
2018-07-04 19:24:43 +0900

    add unlikeableTab layout and modify rankfragment code

--- jungnami/app/src/main/java/sopt_jungnami_android/jungnami/rank/RankFragment.kt
+++ jungnami/app/src/main/java/sopt_jungnami_android/jungnami/rank/RankFragment.kt
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentTransaction
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_rank.*
 import sopt_jungnami_android.jungnami.R
 
 class RankFragment : Fragment() {
@@ -17,10 +18,18 @@ class RankFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         addFragment()
+        rank_frag_unlikeable_tab_btn.setOnClickListener {
+            replaceFragment(UnlikeableTab())
+        }
+
     }
     private fun addFragment(){
         val transaction = childFragmentManager.beginTransaction()
         transaction.add(R.id.rank_frag_fragment_frame_fl, LikeableTab()).commit()
+    }
+    private fun replaceFragment(fragment: Fragment){
+        val transaction = childFragmentManager.beginTransaction()
+        transaction.replace(R.id.rank_frag_fragment_frame_fl, fragment).commit()
     }
 
 }
