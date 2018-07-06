6845f392aaff9876244bbcd2f1ae4941980cd77a
sooyounglim <stellar1198@gmail.com>
2018-07-06 16:07:52 +0900

    add img sources and create legislator's mypage

---
+++ jungnami/app/src/main/java/sopt_jungnami_android/jungnami/LegislatorPage.kt
@@ -0,0 +1,12 @@
+package sopt_jungnami_android.jungnami
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LegislatorPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_legislator_page)
+    }
+}
