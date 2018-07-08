a7727611bcbcf16c2a7c77aa32c940e6f642193e
nanyounan <nanyounan@naver.com>
2018-07-08 18:57:09 +0900

    add swipe refresh

--- jungnami/app/src/main/java/sopt_jungnami_android/jungnami/MainActivity.kt
+++ jungnami/app/src/main/java/sopt_jungnami_android/jungnami/MainActivity.kt
@@ -13,10 +13,13 @@ import sopt_jungnami_android.jungnami.legislator_list.LegislatorListFragment
 import sopt_jungnami_android.jungnami.rank.RankFragment
 
 class MainActivity : AppCompatActivity() {
+    var current_tab_idx : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setStatusBarColor()
+        main_act_rank_btn.isSelected = true
         setBottomNavigationClickListener()
         addFragment(RankFragment())
         //FirebaseConnection().onTokenRefresh()
@@ -25,33 +28,79 @@ class MainActivity : AppCompatActivity() {
 //        activity!!.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
 //        this.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
         main_act_rank_btn.setOnClickListener {
-            it.isSelected = true
+            checkSelectedTabView(0)
             replaceFragment(RankFragment())
         }
         main_act_list_btn.setOnClickListener {
+            checkSelectedTabView(1)
             replaceFragment(LegislatorListFragment())
         }
         main_act_community_btn.setOnClickListener {
+            checkSelectedTabView(2)
             replaceFragment(CommunityFragment())
         }
         main_act_content_btn.setOnClickListener {
+            checkSelectedTabView(3)
             replaceFragment(ContentsFragment())
         }
     }
 
-    fun addFragment(fragment: Fragment): Unit {
+    private fun addFragment(fragment: Fragment): Unit {
         val fm = supportFragmentManager
         val transaction = fm.beginTransaction()
         transaction.add(R.id.main_act_fragment_fl, fragment)
         transaction.commit()
     }
 
-    fun replaceFragment(fragment: Fragment) {
+    private fun replaceFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.main_act_fragment_fl, fragment)
 //        transaction.addToBackStack(null) //백키 눌렀을때 순차대로
         transaction.commit()
     }
+    private fun chagneNonSelectedTabView(idx : Int){
+        when (idx) {
+            0 -> main_act_rank_btn.isSelected = false
+            1 -> main_act_list_btn.isSelected = false
+            2 -> main_act_community_btn.isSelected = false
+            3 -> main_act_content_btn.isSelected = false
+        }
+    }
+
+    private fun chagneSelectedTabView(idx : Int){
+        when (idx) {
+            0 -> main_act_rank_btn.isSelected = true
+            1 -> main_act_list_btn.isSelected = true
+            2 -> main_act_community_btn.isSelected = true
+            3 -> main_act_content_btn.isSelected = true
+        }
+    }
+    private fun checkSelectedTabView(selected_idx : Int){
+        when (selected_idx) {
+            0 -> {
+                chagneNonSelectedTabView(current_tab_idx)
+                current_tab_idx = 0
+                chagneSelectedTabView(current_tab_idx)
+            }
+            1 -> {
+                chagneNonSelectedTabView(current_tab_idx)
+                current_tab_idx = 1
+                chagneSelectedTabView(current_tab_idx)
+            }
+            2 -> {
+                chagneNonSelectedTabView(current_tab_idx)
+                current_tab_idx = 2
+                chagneSelectedTabView(current_tab_idx)
+            }
+            3 -> {
+                chagneNonSelectedTabView(current_tab_idx)
+                current_tab_idx = 3
+                chagneSelectedTabView(current_tab_idx)
+            }
+        }
+    }
+
+
 
     private fun setStatusBarColor() {
         val view: View? = window.decorView
