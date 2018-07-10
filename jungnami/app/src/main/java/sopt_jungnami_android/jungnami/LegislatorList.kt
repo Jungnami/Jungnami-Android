70439b17b543ab0f8db838e5f3bb79140d493a6c
sooyounglim <stellar1198@gmail.com>
2018-07-10 13:31:45 +0900

    write LegislatorList.kt

--- jungnami/app/src/main/java/sopt_jungnami_android/jungnami/LegislatorList.kt
+++ jungnami/app/src/main/java/sopt_jungnami_android/jungnami/LegislatorList.kt
@@ -4,10 +4,15 @@ import android.graphics.Color
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.view.View
 import kotlinx.android.synthetic.main.activity_legislator_list.*
 
+//Written by SooYoung
+
 class LegislatorList : AppCompatActivity(), View.OnClickListener {
+
+    var isFavorableSelected: Boolean = true
 
     override fun onClick(v: View?) {
         when(v) {
@@ -18,21 +23,44 @@ class LegislatorList : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        var isFavorSelected : Boolean = true
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_legislator_list)
         setStatusBarColor()
-
-        legislator_list_act_likeable_tab_btn.setOnClickListener {
-            isFavorSelected = true
-        }
-        legislator_list_act_unlikeable_tab_btn.setOnClickListener {
-            isFavorSelected = false
-        }
+        setOnClickListener()
     }
 
+    private fun setOnClickListener(){
+        legislator_list_act_likeable_tab_btn.setOnClickListener {
+            isFavorableSelected = true
+            isSelectedTabView()
+        }
+        legislator_list_act_unlikeable_tab_btn.setOnClickListener {
+            isFavorableSelected = true
+            isSelectedTabView()
+        }
+        legislator_list_act_search.setOnClickListener {
+            legislator_list_act_legislator_search_rl.visibility = View.VISIBLE
+        }
+        legislator_list_act_is_display_blind_panel_rl.setOnClickListener {
+            legislator_list_act_legislator_search_rl.visibility = View.GONE
+        }
+        // 취소 버튼
+    }
 
+    private fun isSelectedTabView(){
+        if (isFavorableSelected){
+            legislator_list_act_likeable_title_tv.setTextColor(Color.parseColor("#6b6b6b"))
+            legislator_list_act_likable_underbar_line.visibility = View.VISIBLE
+            legislator_list_act_unlikeable_title_tv.setTextColor(Color.parseColor("#d8d8d8"))
+            legislator_list_act_unlikeable_underbar_line.visibility = View.INVISIBLE
+        }
+        else {
+            legislator_list_act_likeable_title_tv.setTextColor(Color.parseColor("#d8d8d8"))
+            legislator_list_act_likable_underbar_line.visibility = View.INVISIBLE
+            legislator_list_act_unlikeable_title_tv.setTextColor(Color.parseColor("#6b6b6b"))
+            legislator_list_act_unlikeable_underbar_line.visibility = View.VISIBLE
+        }
+    }
 
     private fun setStatusBarColor(){
         val view : View? = window.decorView
@@ -42,5 +70,13 @@ class LegislatorList : AppCompatActivity(), View.OnClickListener {
                 window.statusBarColor = Color.parseColor("#FFFFFF")
             }
         }
+    }
+
+    private fun addFragment(){
+
+    }
+
+    private fun replaceFragment() {
+
     }
 }
