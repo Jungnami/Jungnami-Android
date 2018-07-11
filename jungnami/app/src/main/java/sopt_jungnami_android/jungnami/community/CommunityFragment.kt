e5d2f2777f2612f778d765a6c7d1db79bca8cad7
Tak Hyungmin <milkho11@naver.com>
2018-07-11 18:26:19 +0900

    modify_Community_Fragment_kotlin

--- jungnami/app/src/main/java/sopt_jungnami_android/jungnami/community/CommunityFragment.kt
+++ jungnami/app/src/main/java/sopt_jungnami_android/jungnami/community/CommunityFragment.kt
@@ -7,6 +7,7 @@ import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_community.*
 import org.jetbrains.anko.support.v4.startActivity
 import retrofit2.Call
@@ -17,20 +18,44 @@ import sopt_jungnami_android.jungnami.CommunityWritePage
 import sopt_jungnami_android.jungnami.Get.GetCommunityFeedResponse
 import sopt_jungnami_android.jungnami.Network.ApplicationController
 import sopt_jungnami_android.jungnami.Network.NetworkService
+import sopt_jungnami_android.jungnami.Post.PostCommunityLikeRequset
 import sopt_jungnami_android.jungnami.R
 import sopt_jungnami_android.jungnami.data.Content
 import sopt_jungnami_android.jungnami.mypage.MyPageActivity
 
-class CommunityFragment : Fragment() {
-    lateinit var networkService: NetworkService
+class CommunityFragment : Fragment(), View.OnClickListener {
+
     lateinit var feedDataList : ArrayList<Content>
     lateinit var communityRecyclerViewAdapter: CommunityRecyclerViewAdapter
+    lateinit var networkService: NetworkService
+    lateinit var postCommunityLikeRequset : PostCommunityLikeRequset
+
+    override fun onClick(v: View?) {
+
+//        val index : Int = community_frag_feed_list_rv.getChildAdapterPosition(v!!) // 몇번째를 클릭했는지에 대한 정보를 index가 가지고 있다.
+//        val name : String = kakaoItems[index].name
+//        val profile : Int = kakaoItems[index].profile
+//        val intent : Intent = Intent(applicationContext, ChatActivity::class.java)
+//        // 인텐트로 정보 전달
+//        // 인텐트로 담아서 키값으로 데이터 넘어감
+//
+//        intent.putExtra("name",name)
+//        intent.putExtra("profile",profile)
+//
+//        startActivity(intent)
+//        // 리사이클러뷰의 몇번째를 클릭했는지 처리할 것
+    }
+
+
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_community, container, false)
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -44,6 +69,17 @@ class CommunityFragment : Fragment() {
 //        setRecyclerViewAdapter()
     }
 
+    private fun setRecyclerViewAdapter() {
+        communityRecyclerViewAdapter = CommunityRecyclerViewAdapter(feedDataList, activity)
+        communityRecyclerViewAdapter.setOnItemClickListener(this)
+        community_frag_feed_list_rv.adapter = communityRecyclerViewAdapter
+        community_frag_feed_list_rv.layoutManager = LinearLayoutManager(activity)
+    }
+
+//    fun postCommunityLike(){
+//        val postCommunityLikeRequset = PostCommunityLikeRequset()
+//    }
+
 
     fun getCommunityFeed(){
         val getCommunityFeedResponse = networkService.getCommunityFeed()
@@ -54,11 +90,18 @@ class CommunityFragment : Fragment() {
             override fun onResponse(call: Call<GetCommunityFeedResponse>?, response: Response<GetCommunityFeedResponse>?) {
                 if(response!!.isSuccessful){
 
+
+
+                    var user_img_url = response!!.body()!!.data!!.user_img_url
+
+
+                    Glide.with(context!!)
+                            .load(user_img_url)
+                            .into(community_frag_my_picture_iv)
+
                     var alarmcnt = response!!.body()!!.data!!.alarmcnt
                     feedDataList = response!!.body()!!.data!!.content as ArrayList<Content>
-                    communityRecyclerViewAdapter = CommunityRecyclerViewAdapter(feedDataList, activity)
-                    community_frag_feed_list_rv.adapter = communityRecyclerViewAdapter
-                    community_frag_feed_list_rv.layoutManager = LinearLayoutManager(activity)
+                    setRecyclerViewAdapter()
                 }
             }
 
