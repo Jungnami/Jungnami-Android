62c180ede2e07684f0bb706e2abc2d166789915f
sooyounglim <stellar1198@gmail.com>
2018-07-13 04:53:39 +0900

    modify party/district legislator connector

--- jungnami/app/src/main/java/sopt_jungnami_android/jungnami/Get/GetPartyDistrictLegislatorListResponse.kt
+++ jungnami/app/src/main/java/sopt_jungnami_android/jungnami/Get/GetPartyDistrictLegislatorListResponse.kt
@@ -3,6 +3,6 @@ package sopt_jungnami_android.jungnami.Get
 import sopt_jungnami_android.jungnami.data.PartyDistrictLegistlatorListData
 
 data class GetPartyDistrictLegislatorListResponse (
-        var data: List<PartyDistrictLegistlatorListData>,
+        var data: PartyDistrictLegistlatorListData,
         var message: String
 )
